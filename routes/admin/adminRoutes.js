a1fd7cb3f4567c44a0f7f74feaa7c5a1a9c71f97
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-09-27 15:25:05 -0600

    adding new features

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -5,6 +5,7 @@ const {
   getPosts,
   sumbitPosts,
   createPosts,
+  editPosts,
 } = require("../../controllers/adminControllers");
 
 router.all("/*", (request, _response, next) => {
@@ -18,5 +19,7 @@ router.route("/").get(index);
 router.route("/posts").get(getPosts);
 
 router.route("/posts/create").get(createPosts).post(sumbitPosts);
+
+router.route("/posts/edit/:id").get(editPosts);
 
 module.exports = router;
